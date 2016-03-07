7d2ef5def5cd339d0cf4b3f675afae875cb92dfe
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2016-03-06 19:24:59 -0800

    Added jQuery plugins: (1) Sticky Header, which sticks the navigation div to the top of the screen, and (2) Slick carousel, as an image slider for the three banners of mt. bachlor, sparks lake, and mirror pond.

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.main').slick({ 
+  	dots: true,
+  	mobileFirst: true
+  }); // end of slick carousel
+
+  $('#top-nav-id').sticky({  }); // end of sticky header
+});
